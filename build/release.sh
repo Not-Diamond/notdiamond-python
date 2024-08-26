5fb7a3e5a548a306df6edec8b4850d7b9f8982b2
Alejandro Companioni <a9@notdiamond.ai>
2024-08-26 14:55:39 -0400

    add build script

---
+++ build/release.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+EXPECTED_ARGS=1
+if [ $# -ne $EXPECTED_ARGS ]; then
+    echo "Invalid number of arguments. Expected: $EXPECTED_ARGS"
+    exit 1
+fi
+
+VALID_VERSIONS=("major" "minor" "patch")
+if [[ ! " ${VALID_VERSIONS[@]} " =~ " $1 " ]]; then
+    echo "Invalid version type. Expected one of: ${VALID_VERSIONS[*]}"
+    exit 1
+fi
+
+git checkout main && git pull
+poetry version $1
+git add pyproject.toml
+git commit -m "Bump version to $TAG_NAME"
+git push origin main
+
+TAG_NAME=$(poetry version -s)
+git tag $TAG_NAME && git push origin tag $TAG_NAME
