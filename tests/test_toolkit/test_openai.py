8dd9a4f3c89f04a9e6754622b5467411ded45b61
Alejandro Companioni <a9@notdiamond.ai>
2024-09-10 13:39:00 -0400

    check streaming

--- tests/test_toolkit/test_openai.py
+++ tests/test_toolkit/test_openai.py
@@ -1,3 +1,9 @@
+"""
+Tests for our OpenAI client wrapper. Most of these ensure that the client supports
+capabilities defined in the API reference:
+
+https://platform.openai.com/docs/api-reference/introduction
+"""
 from notdiamond.settings import OPENAI_API_KEY
 from notdiamond.toolkit.openai import OpenAI
 
@@ -9,10 +15,19 @@ def test_openai_init():
     )
     assert client is not None
 
+    client2 = OpenAI(
+        api_key=OPENAI_API_KEY,
+        base_url="https://api.openai.com/v1",
+        llm_configs=["openai/gpt-4o", "openai/gpt-4o-mini"],
+    )
+    assert client2 is not None
+
     client3 = OpenAI(
         api_key=OPENAI_API_KEY,
         base_url="https://api.openai.com/v1",
         llm_configs=["openai/gpt-4o", "openai/gpt-4o-mini"],
+        organization="nd-oai-organization",
+        project="nd-oai-project",
     )
     assert client3 is not None
 
@@ -83,3 +98,24 @@ def test_openai_chat_completions_create():
     )
     assert all_oai_model_response is not None
     assert len(all_oai_model_response.choices[0].message.content) > 0
+
+
+def test_openai_chat_completions_create_stream():
+    model_response_stream = OpenAI(
+        api_key=OPENAI_API_KEY
+    ).chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": "Hello, world! Does this chat.completions.create call work?",
+            }
+        ],
+        stream=True,
+    )
+
+    any_nonempty_chunk = False
+    for chunk in model_response_stream:
+        if chunk.choices[0].delta.content is not None:
+            any_nonempty_chunk = True
+            break
+    assert any_nonempty_chunk
