a229377b53bf24935e912c854ff0324f093e9322
Alejandro Companioni <a9@notdiamond.ai>
2024-10-03 12:59:59 -0400

    LiteLLM MIT license notice

--- notdiamond/toolkit/litellm/main.py
+++ notdiamond/toolkit/litellm/main.py
@@ -1,5 +1,32 @@
 # flake8: noqa
 
+# This file is a modified version of the original module provided by BerriAI.
+# We have modified the file to add support for Not Diamond, and include the following
+# license to comply with their license requirements:
+
+# MIT License
+
+# Copyright (c) 2023 Berri AI
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 import asyncio
 import contextvars
 import inspect
