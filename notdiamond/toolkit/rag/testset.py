bed2b2a8977b7bfffcbd6734007e76b47af2bb4d
Tze-Yang Tung <r0ymanesco@gmail.com>
2024-11-20 11:22:13 -0500

    bugfixes

--- notdiamond/toolkit/rag/testset.py
+++ notdiamond/toolkit/rag/testset.py
@@ -10,14 +10,18 @@ from llama_index.core.base.embeddings.base import (
 )
 from llama_index.core.base.llms.base import BaseLLM as LlamaIndexLLM
 from llama_index.core.schema import Document as LlamaIndexDocument
-from ragas.embeddings import BaseRagasEmbeddings
-from ragas.llms import BaseRagasLLM
+from ragas.embeddings import BaseRagasEmbeddings, LlamaIndexEmbeddingsWrapper
+from ragas.llms import BaseRagasLLM, LlamaIndexLLMWrapper
 from ragas.run_config import RunConfig
 from ragas.testset import TestsetGenerator
-from ragas.testset.graph import KnowledgeGraph
+from ragas.testset.graph import KnowledgeGraph, Node, NodeType
 from ragas.testset.persona import Persona
 from ragas.testset.synthesizers import QueryDistribution
-from ragas.testset.transforms import Transforms
+from ragas.testset.transforms import (
+    Transforms,
+    apply_transforms,
+    default_transforms,
+)
 
 
 class TestDataGenerator(TestsetGenerator):
@@ -85,3 +89,86 @@ class TestDataGenerator(TestsetGenerator):
             return dataset.to_pandas()
 
         raise Exception(f"Document type {type(documents[0])} not supported.")
+
+    def generate_with_llamaindex_docs(
+        self,
+        documents: Sequence[LlamaIndexDocument],
+        testset_size: int,
+        transforms: Optional[Transforms] = None,
+        transforms_llm: Optional[LlamaIndexLLM] = None,
+        transforms_embedding_model: Optional[LlamaIndexEmbedding] = None,
+        query_distribution: Optional[QueryDistribution] = None,
+        run_config: Optional[RunConfig] = None,
+        callbacks: Optional[Callbacks] = None,
+        with_debugging_logs=False,
+        raise_exceptions: bool = True,
+    ):
+        """
+        Generates an evaluation dataset based on given scenarios and parameters.
+        """
+
+        run_config = run_config or RunConfig()
+
+        # force the user to provide an llm and embedding client to prevent use of default LLMs
+        if not self.llm and not transforms_llm:
+            raise ValueError(
+                "An llm client was not provided."
+                " Provide an LLM on TestsetGenerator instantiation or as an argument for transforms_llm parameter."
+                " Alternatively you can provide your own transforms through the `transforms` parameter."
+            )
+        if not self.embedding_model and not transforms_embedding_model:
+            raise ValueError(
+                "An embedding client was not provided."
+                " Provide an embedding through the transforms_embedding_model parameter."
+                " Alternatively you can provide your own transforms through the `transforms` parameter."
+            )
+
+        if not transforms:
+            # use TestsetGenerator's LLM and embedding model if no transforms_llm or transforms_embedding_model is provided
+            if transforms_llm is None:
+                llm_for_transforms = self.llm
+            else:
+                llm_for_transforms = LlamaIndexLLMWrapper(transforms_llm)
+            if transforms_embedding_model is None:
+                embedding_model_for_transforms = self.embedding_model
+            else:
+                embedding_model_for_transforms = LlamaIndexEmbeddingsWrapper(
+                    transforms_embedding_model
+                )
+
+            # create the transforms
+            transforms = default_transforms(
+                documents=[
+                    LCDocument(page_content=doc.text) for doc in documents
+                ],
+                llm=llm_for_transforms,
+                embedding_model=embedding_model_for_transforms,
+            )
+
+        # convert the documents to Ragas nodes
+        nodes = []
+        for doc in documents:
+            if doc.text is not None and doc.text.strip() != "":
+                node = Node(
+                    type=NodeType.DOCUMENT,
+                    properties={
+                        "page_content": doc.text,
+                        "document_metadata": doc.metadata,
+                    },
+                )
+                nodes.append(node)
+
+        kg = KnowledgeGraph(nodes=nodes)
+
+        # apply transforms and update the knowledge graph
+        apply_transforms(kg, transforms, run_config)
+        self.knowledge_graph = kg
+
+        return self.generate(
+            testset_size=testset_size,
+            query_distribution=query_distribution,
+            run_config=run_config,
+            callbacks=callbacks,
+            with_debugging_logs=with_debugging_logs,
+            raise_exceptions=raise_exceptions,
+        )
