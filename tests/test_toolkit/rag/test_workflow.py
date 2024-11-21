2bfec236ae757bebf5e2fa9d1e06a4f98597d41a
Alejandro Companioni <a9@notdiamond.ai>
2024-11-21 15:46:40 -0500

    initial tests

---
+++ tests/test_toolkit/rag/test_workflow.py
@@ -0,0 +1,21 @@
+from typing import Annotated
+
+import pandas as pd
+import pytest
+
+from notdiamond.toolkit.rag.workflow import BaseNDRagWorkflow, IntValueRange
+
+
+class TestNDRagWorkflow(BaseNDRagWorkflow):
+    parameter_specs = {
+        "chunk_size": (Annotated[int, IntValueRange(1000, 2500, 500)], 1000)
+    }
+
+
+def test_set_param_values():
+    workflow = TestNDRagWorkflow(pd.DataFrame())
+    workflow._set_param_values({"chunk_size": 1500})
+    assert workflow.chunk_size == 1500
+
+    with pytest.raises(ValueError):
+        workflow._set_param_values({"nonexistent_param": 1500})
